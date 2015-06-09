2a42d0bf11a783925b8e6f64bcac38cae80c0acc
Mark Terry <mark.terry@holidayextras.com>
2015-06-09 10:33:11 +0100

    removed redundant grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,31 +13,10 @@ module.exports = function( grunt ) {
 			sample: {
 				src: [ 'configs/*.json' ]
 			}
-		},
-		jshint: {
-			options: {
-				jshintrc: 'configs/jshintrc.json'
-			},
-			core: {
-				src: ['*.js']
-			},
-			test: {
-				src: ['test/**/*.js']
-			}
-		},
-		jscs: {
-			options: {
-				config: 'configs/jscsrc.json',
-				force: 'true'
-			},
-			src: ['<%= jshint.core.src %>', '<%= jshint.test.src %>']
 		}
-
 	} );
 
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-yaml-validator' );
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-jscs' );
 
 };
