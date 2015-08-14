c88959b1b11c4f6c60a12b088096a026910e42ab
Mark Terry <mark.terry@holidayextras.com>
2015-08-14 15:53:18 +0100

    added eslint config script

---
+++ script/calculate_eslint_config.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+var CLIEngine = require('eslint').CLIEngine;
+
+if (!process.argv[2]) {
+  console.log('Usage: ' + process.argv[1] + ' <config file>');
+  process.exit(1);  // eslint-disable-line no-process-exit
+}
+
+var cli = new CLIEngine({
+  configFile: process.argv[2],
+  useEslintrc: false
+});
+
+console.log(cli.getConfigForFile('index.js'));
