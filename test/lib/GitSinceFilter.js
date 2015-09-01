5747ef4b5149b339df179705ae45a2b02f4798a3
Mark Terry <mark.terry@holidayextras.com>
2015-09-02 00:38:32 +0100

    care about the format not the date, as CI runs on UTC

--- test/lib/GitSinceFilter.js
+++ test/lib/GitSinceFilter.js
@@ -58,7 +58,7 @@ describe('GitSinceFilter', function() {
     context('with a date string format only Date() knows about', function() {
 
       it('formats the string in an ISO format', function() {
-        GitSinceFilter._formatDate('Tue Jun 09 2015 14:49:57 GMT+0100 (BST)').should.equal('2015-06-09T14:49:57+01:00');
+        GitSinceFilter._formatDate('Tue Jun 09 2015 14:49:57 GMT+0100 (BST)').should.match(/\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:\d{2}\+\d{2}\:\d{2}/);
       });
 
     });
