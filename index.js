6922b46943e362bd846f159e19c92103d5c6812a
Mark Terry <mark.terry@holidayextras.com>
2015-12-14 11:17:10 +0000

    now passing streams back to the caller

--- index.js
+++ index.js
@@ -18,14 +18,18 @@ makeUp.path = function(item) {
 };
 
 makeUp.check = function(options, callback) {
-  async.map(this.checkIntegrations, this._runIntegration.bind(undefined, options), callback);
+  async.map(this.checkIntegrations, this._runIntegration.bind(undefined, options), function(error, allStreams) {
+    allStreams.forEach(function(stream) {
+      console.log(stream.toString());
+    });
+    callback(error);
+  });
 };
 
 makeUp._runIntegration = function(options, item, callback) {
   var intStream = new streams.WritableStream();
   intStream.write('\n' + item.label + '\n==============\n');
   item.run(options, intStream, function(err, result) {
-    console.log(intStream.toString());
     callback(err, result);
   });
 };
