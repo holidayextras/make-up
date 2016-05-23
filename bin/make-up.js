711817336ab7f69ed0cbc1888d25c70b07fa1115
Pedro Romano <pedro.romano@holidayextras.com>
2016-05-23 09:58:25 +0100

    Make sure we always print the result if it exists.

--- bin/make-up.js
+++ bin/make-up.js
@@ -13,9 +13,11 @@ var options = {
   gitBranch: argv.b
 };
 MakeUp.check(options, function(err, result) {
+  if (result) {
+    console.log(result);
+  }
   if (err) {
     console.log('ERROR: ' + err.message);
     process.exit(1); // eslint-disable-line no-process-exit
   }
-  console.log(result);
 });
